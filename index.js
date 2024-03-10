ac130b92fb23f3f804081142c3c4c196eb75194e
madhu <rayi.madhupriya@gmail.com>
2024-03-10 15:27:56 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express= require('express');
+const morgan= require('morgan');
+const {createProxyMiddleware} = require('http-proxy-middleware');
+const rateLimit= require('express-rate-limit');
+const  axios= require('axios');
+
+const app= express();
+
+const PORT=3006;
+
+const limiter= rateLimit({
+    windowMs: 2 * 60 * 1000, //15 minutes
+    max: 5, //Limit ech IP to 100 request per window (here, per 15 minutes)
+})
+app.use(morgan('combined'));
+app.use(limiter);
+app.use('/bookingservice',async(req,res,next)=>{
+    console.log(req.headers['x-access-token']);
+    try{
+        const response= await axios.get('http://localhost:3002/api/v1/isauthenticated',{
+        headers:{
+            'x-access-token': req.headers['x-access-token']
+        }
+        
+    });
+    console.log(response.data);
+    if(response.data.success){
+        next();
+    }       
+    else{
+        return res.status(401).json({
+            message:"Unauthorized"
+        })
+    }
+    }catch(error){
+        return res.status(401).json({
+            message:"Unauthorized"
+        })
+    }
+
+})
+app.use('/bookingservice',createProxyMiddleware({target:'http://localhost:3003'}))
+app.get('/home',(req,res)=>{
+    return res.json({
+        message: 'OK'
+    })
+})
+
+app.listen(PORT,()=>{
+    console.log("Server started at port",PORT);
+});
